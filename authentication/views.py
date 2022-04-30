507f5bdfda459c105f7da435a8778e23c52fe50d
Christopher Whitelam <cwhitelam70@gmail.com>
2022-04-29 23:26:48 -0400

    updated README.md

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import authentication
+
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("hello, I am working")
+    return render(request, "authentication/index.html")
+
+
+def register(request):
+    return render(request, "authentication/register.html")
+
+
+def login(request):
+    return render(request, "authentication/login.html")
+
+
+def logout(request):
+    pass
