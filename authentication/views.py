efb345502da810e363c008de7e0d041b5961173c
Christopher Whitelam <cwhitelam70@gmail.com>
2022-05-17 19:21:29 -0400

    added login function

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 import authentication
 
@@ -11,6 +13,25 @@ def home(request):
 
 
 def register(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create(username,email, pass1)
+        mysuer.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(request, "Your account has been successfully created.")
+
+        return redirect('login')
+
     return render(request, "register.html")
 
 
